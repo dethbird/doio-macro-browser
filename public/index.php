760406b43967982bda3e115551000dc8b326ad4b
Rishi Satsangi <rishi.satsangi@gmail.com>
2025-12-03 16:14:15 +0000

    add twig templating

--- public/index.php
+++ public/index.php
@@ -5,12 +5,17 @@ require __DIR__ . '/../vendor/autoload.php';
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
 
 $app = AppFactory::create();
 
+$twig = Twig::create(__DIR__ . '/../templates', ['cache' => false]);
+$app->add(TwigMiddleware::create($app, $twig));
+
 $app->get('/', function (Request $request, Response $response) {
-    $response->getBody()->write('hello world');
-    return $response;
+    $view = Twig::fromRequest($request);
+    return $view->render($response, 'index.twig');
 });
 
 $app->run();
